acc52c4cd523aaaf8e0c6d86c8f15bd9240b53ae
tjfh3859 <70091697+tjfh3859@users.noreply.github.com>
2021-05-05 14:00:42 +0900

    Middleware
    middleware: 중간에 있는 소프트웨어,
    request -> middleware -> response
    next argument: 다음 함수 호출
    app.use -> global middleware를 만듦
    middleware를 use하는게 먼저, 다음으로 url의 get

--- src/server.js
+++ src/server.js
@@ -4,6 +4,20 @@ const PORT = 4000;
 
 const app = express();
 
+const logger = (req, res, next) => {
+  console.log(`${req.method} ${req.url}`);
+  next();
+};
+
+const privateMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    return res.send("Not Allowed");
+  }
+  console.log("Allowed");
+  next();
+};
+
 const handleHome = (req, res) => {
   //console.log(req);
   //console.log(res);
@@ -14,8 +28,16 @@ const handleLogin = (req, res) => {
   return res.send("Login");
 };
 
+const handleProtected = (req, res) => {
+  return res.send("Private Page");
+};
+
+app.use(logger);
+app.use(privateMiddleware);
+
 app.get("/", handleHome);
 app.get("/login", handleLogin);
+app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT}`);
