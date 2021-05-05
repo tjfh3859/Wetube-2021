5fedad7d96add3524669ecd2626260e0eb11b7aa
tjfh3859 <70091697+tjfh3859@users.noreply.github.com>
2021-05-05 14:58:28 +0900

    i morgan
    morgan: nodejs용 request logger middleware
    ("dev"):GET path statuscode 응답시간

--- src/server.js
+++ src/server.js
@@ -1,22 +1,10 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
-
-const logger = (req, res, next) => {
-  console.log(`${req.method} ${req.url}`);
-  next();
-};
-
-const privateMiddleware = (req, res, next) => {
-  const url = req.url;
-  if (url === "/protected") {
-    return res.send("Not Allowed");
-  }
-  console.log("Allowed");
-  next();
-};
+const logger = morgan("dev");
 
 const handleHome = (req, res) => {
   //console.log(req);
@@ -28,16 +16,10 @@ const handleLogin = (req, res) => {
   return res.send("Login");
 };
 
-const handleProtected = (req, res) => {
-  return res.send("Private Page");
-};
-
 app.use(logger);
-app.use(privateMiddleware);
 
 app.get("/", handleHome);
 app.get("/login", handleLogin);
-app.get("/protected", handleProtected);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT}`);
