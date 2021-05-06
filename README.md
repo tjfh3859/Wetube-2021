e54579476c7a5a33cd86e6d6bed54ca624c5de24
tjfh3859 <70091697+tjfh3859@users.noreply.github.com>
2021-05-06 14:31:30 +0900

    Add Router
    Router: 컨트롤러와 URL 관리를 쉽게해줌
    
    const globalRouter = express.Router(); -> Router 만들기
    const handle = (req, res) => res.express(); -> handle 만들기
    globalRouter.get("URL", handle); -> 해당 URL에 handle 적용
    
    app.use("URL", globalRouter); -> 해당 URL접근시 globalRouter 안에 있는 컨트롤러 탐색
    
    파일은 독립적 import와 export를 통해 연결

---
+++ README.md
@@ -0,0 +1,10 @@
+# Clone Youtube
+
+- Nomad Coders lectures
+
+# 참고 사이트 및 다운로드
+
+https://nodejs.org/ko/
+https://www.npmjs.com/
+https://developer.mozilla.org/ko/docs/Web/JavaScript
+https://expressjs.com/ko/4x/api.html#app
--- src/server.js
+++ src/server.js
@@ -1,25 +1,19 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
 
-const handleHome = (req, res) => {
-  //console.log(req);
-  //console.log(res);
-  return res.send("Hello");
-};
-
-const handleLogin = (req, res) => {
-  return res.send("Login");
-};
-
 app.use(logger);
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
+app.use("/", globalRouter);
+app.use("/users", userRouter);
+app.use("/videos", videoRouter);
 
 const handleListening = () =>
   console.log(`✅ Server listening on port http://localhost:${PORT}`);
