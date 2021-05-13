fcdbd1002ec24e1c2f4992a4cea92c4022af82a5
tjfh3859 <70091697+tjfh3859@users.noreply.github.com>
2021-05-13 14:19:37 +0900

    GET, POST
    form(method="POST"): method를 통해 데이터를 post (method 기본값은 GET)
    form(action=""): action은 데이터를 특정 url로 보냄
    
    get: url 이동 후 tile이 url에 포함됨, 검색등에 사용(데이터를 받는게 목적)
    post: 파일을 보내거나 database에 있는 값을 바꾸는데 사용
    
    express.urlencoded: form의 body를 이해하게 만듦

--- src/server.js
+++ src/server.js
@@ -9,10 +9,11 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 
-app.use(logger);
-
 app.set("view engine", "pug");
 app.set("views", "src/views");
+
+app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
