6244f64c2c58cebc696840f0c8738410a4d546ca
tjfh3859 <70091697+tjfh3859@users.noreply.github.com>
2021-06-07 12:13:00 +0900

    Error check
    try catch -> 에러확인
    errorMessage -> 에러 pug 보내기
    
    default: Date.now ->  ()넣으면 function을 바로 실행시킴, mongoose가 자동으로 ()넣어줌

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -5,7 +5,7 @@ import { join, login } from "../controllers/userController";
 const globalRouter = express.Router();
 
 globalRouter.get("/", home);
-globalRouter.get("join", join);
+globalRouter.get("/join", join);
 globalRouter.get("/login", login);
 
 export default globalRouter;
