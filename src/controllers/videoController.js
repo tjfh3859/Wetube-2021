4d3a06ba7f9667241fe7c193e22fefb472b1c572
tjfh3859 <70091697+tjfh3859@users.noreply.github.com>
2021-06-07 16:09:19 +0900

    middleware and static
    https://mongoosejs.com/docs/middleware.html: mongoose middleware
    
    mongoose middleware: Model이 저장되기전에 작성, 실행
    mongoose내에서 function 제작-> export도 가능
    static funtion을 이용(formatHashtags)

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -47,9 +47,7 @@ export const postEdit = async (req, res) => {
   await Video.findByIdAndUpdate(id, {
     title,
     description,
-    hastags: hashtags
-      .split(",")
-      .map((word) => (word.startsWith("#") ? word : `#${word}`)),
+    hashtags: Video.formatHashtags(hashtags),
   });
   return res.redirect(`/videos/${id}`);
 };
@@ -63,7 +61,7 @@ export const postUpload = async (req, res) => {
     await Video.create({
       title,
       description,
-      hashtags: hashtags.split(",").map((word) => `#${word}`),
+      hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
