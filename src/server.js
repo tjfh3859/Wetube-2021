5849dbd028c66e07d6fe55e5c7bbc42063f13b09
tjfh3859 <70091697+tjfh3859@users.noreply.github.com>
2021-06-11 09:02:28 +0900

    Cookie
    npm i express-session: express에서 세션처리를 가능하게 해주는 middleware,유저를 저장

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import globalRouter from "./routers/rootRouter";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
@@ -12,6 +13,25 @@ app.set("views", "src/views");
 
 app.use(logger);
 app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  session({
+    secret: "Hello!",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+app.use((req, res, next) => {
+  req.sessionStore.all((error, sessions) => {
+    console.log(sessions);
+    next();
+  });
+});
+
+app.get("/add-one", (req, res, next) => {
+  return res.send(`${req.session.id}`);
+});
 
 app.use("/", globalRouter);
 app.use("/users", userRouter);
