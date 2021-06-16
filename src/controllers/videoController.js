f37a93d55b3f3af1bd940c820799c9fd670466a8
tjfh3859 <70091697+tjfh3859@users.noreply.github.com>
2021-06-16 19:07:28 +0900

    Upload Video

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -56,16 +56,18 @@ export const getUpload = (req, res) => {
   return res.render("upload", { pageTitle: "Upload Video" });
 };
 export const postUpload = async (req, res) => {
+  const { path: fileUrl } = req.file;
   const { title, description, hashtags } = req.body;
   try {
     await Video.create({
       title,
       description,
+      fileUrl,
       hashtags: Video.formatHashtags(hashtags),
     });
     return res.redirect("/");
   } catch (error) {
-    return res.status(400).paramsrender("upload", {
+    return res.status(400).render("upload", {
       pageTitle: "Upload Video",
       errorMessage: error._message,
     });
